48f03048536d559a1eee07dfcbd67884029b39b9
elisagc <elisagcubero@gmail.com>
2025-02-19 13:52:54 +0100

    feat: configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['titantrays.com'],
+  },
 };
 
 export default nextConfig;
